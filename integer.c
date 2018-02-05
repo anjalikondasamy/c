2b39cb08f99054eb8c996adafc508dbdff868ffe
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-05 16:35:38 +0530

    c pgm
    good

---
+++ integer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n, i, sum = 0;
+    clrscr();
+   
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    
+    for(i=0; i <= n; ++i)
+    {
+        sum += i;   
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
